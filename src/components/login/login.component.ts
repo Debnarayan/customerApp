2f9344701f1d028fefcca8be3114ac56ff4969da
Debnarayan <debnarayan.cs@gmail.com>
2017-09-13 15:18:16 +0530

    Third Commit

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 /**
  * Generated class for the LoginComponent component.
@@ -10,13 +11,43 @@ import { Component } from '@angular/core';
   selector: 'app-login',
   templateUrl: 'login.component.html'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
-  text: string;
+  loginForm: FormGroup;
 
-  constructor() {
-    console.log('Hello LoginComponent Component ');
-    this.text = 'Hello World';
+  constructor(private formBuilder: FormBuilder) {
+    console.log('Hello LoginComponent Component');
+  }
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required,
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ]))
+    });
+  }
+
+  validationMessages = {
+    'email': [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'pattern', message: 'Enter a valid email.' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required.' },
+      { type: 'minlength', message: 'Password must be at least 5 characters long.' },
+      { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, and one number.' }
+    ]
+  };
+
+  onSubmit(formValue) {
+    console.log(formValue);
+    console.log(this.loginForm.value)
   }
 
 }
